01be45558813713fcb80609a50d78ac4b16b3009
noahhermann <noah.hermann@bbv.ch>
2025-01-20 16:56:30 +0100

    all event types handled

--- aihub_agent/playground/minimal_workflow/semantic_workflow/tests/test_SemanticEventAgent.py
+++ aihub_agent/playground/minimal_workflow/semantic_workflow/tests/test_SemanticEventAgent.py
@@ -38,3 +38,18 @@ async def test_start(agent_runner: AgentTestRunner):
 @then("a StartEvent is present")
 def test_start_present(agent_runner: AgentTestRunner):
     assert agent_runner.has_start_event, "Agent did not receive start event"
+
+
+@then("a RetrieverEvent is present")
+def test_retrieve_present(agent_runner: AgentTestRunner):
+    assert agent_runner.has_start_event, "Agent did not receive retriever event"
+
+
+@then("a RerankerEvent is present")
+def test_rerank_present(agent_runner: AgentTestRunner):
+    assert agent_runner.has_start_event, "Agent did not receive reranker event"
+
+
+@then("a LLMStopEvent is present")
+def test_retrieve_present(agent_runner: AgentTestRunner):
+    assert agent_runner.has_stop_event, "Agent did not stop start event"
