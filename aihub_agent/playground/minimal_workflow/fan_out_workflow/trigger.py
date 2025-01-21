db8c93534b8da038bb1ec90645a0b9ff77050db1
noahhermann <noah.hermann@bbv.ch>
2025-01-21 14:55:05 +0100

    removed changes to fan out from this branch

--- aihub_agent/playground/minimal_workflow/fan_out_workflow/trigger.py
+++ aihub_agent/playground/minimal_workflow/fan_out_workflow/trigger.py
@@ -15,16 +15,12 @@ async def main():
         agent_config=FanOutAgentConfig(
             agent_id="fan_out_agent",
             name=LocaleString(en="Fan Out Agent"),
-            description=LocaleString(
-                en="This is an agent that fans out multiple steps"
-            ),
+            description=LocaleString(en="This is an agent that fans out multiple steps"),
             system_prompt=LocaleString(en="You are an agent"),
         ),
     )
     async with runner.test_run() as topic:
-        await runner.send_event_from_topic(
-            topic=topic, start_event=StartEvent(messages=[])
-        )
+        await runner.send_event_from_topic(topic=topic, start_event=StartEvent(messages=[]))
 
 
 if __name__ == "__main__":
