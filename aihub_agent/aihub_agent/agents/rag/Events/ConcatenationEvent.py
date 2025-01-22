4b074a569c7ac6e0d56d143f4b144d51ae4a99aa
noahhermann <noah.hermann@bbv.ch>
2025-01-22 11:42:40 +0100

    initial commit

--- aihub_agent/aihub_agent/agents/rag/Events/ConcatenationEvent.py
+++ aihub_agent/aihub_agent/agents/rag/Events/ConcatenationEvent.py
@@ -4,6 +4,7 @@ from llama_index.core.schema import NodeWithScore
 from pydantic import Field
 
 from aihub_lib.nats.events import ControlEvent
+from aihub_lib.nats.events.semantic.retriever import Document
 
 
 class ConcatenationEvent(ControlEvent):
@@ -11,4 +12,4 @@ class ConcatenationEvent(ControlEvent):
     Allows for concatenation of retrieved documents.
     """
 
-    nodes: List[NodeWithScore] = Field(..., description="The message including the context nodes information in order.")
+    nodes: List[Document] = Field(..., description="The message including the context nodes information in order.")
--- aihub_agent/aihub_agent/agents/rag/MultiHopRAGAgent.py
+++ aihub_agent/aihub_agent/agents/rag/MultiHopRAGAgent.py
@@ -1,33 +1,37 @@
-from aihub_agent.agents.rag.Events.ConcatenationEvent import ConcatenationEvent
-from aihub_agent.workflow.annotations.custom_types.ListOfSize import FixedList
-from aihub_lib.generative_ai.utils.combine_nodes_in_order import combine_nodes_in_order
-from aihub_lib.generative_ai.utils.condense_standalone_question import condense_standalone_question
-from aihub_lib.generative_ai.utils.limit_chat_history import limit_chat_history
-from aihub_lib.generative_ai.utils.limit_chat_history_with_context import limit_chat_history_with_context
-from aihub_lib.generative_ai.utils.retrieve_nodes import retrieve_nodes
-from aihub_lib.i18n.LocaleHandler import LocaleHandler
-from aihub_lib.nats.context.run.RunContext import RunContext
-from aihub_lib.nats.events.control.start import StartEvent
-from aihub_lib.nats.events.control.stop import StopEvent
-from aihub_lib.nats.events.semantic.llm import LLMEvent
-from aihub_lib.nats.events.semantic.retriever import RetrieverEvent
-from aihub_lib.nats.events.user import UserMessageEvent
-from llama_index.core.base.llms.types import ChatMessage, MessageRole
+from typing import List
 
-from aihub_agent.agents.rag.RAGAgent import RAGAgent
+from llama_index.core.base.llms.types import MessageRole, ChatMessage
+
+from aihub_agent.agents.abstract.Agent import Agent
+from aihub_agent.agents.rag.Configs import MultiHopRAGAgentConfig
 from aihub_agent.agents.rag.Configs.RAGAgentConfig import RAGAgentConfig
 from aihub_agent.agents.rag.Configs.RetrieveStepConfig import RetrieveStepConfig
+from aihub_agent.agents.rag.Events.ConcatenationEvent import ConcatenationEvent
+from aihub_agent.agents.rag.Events.DecomposeQueryEvent import DecomposeQueryEvent
 from aihub_agent.agents.rag.Events.InOrderNodeCombinerEvent import InOrderNodeCombinerEvent
 from aihub_agent.agents.rag.Events.LimitChatHistoryEvent import LimitChatHistoryEvent
 from aihub_agent.agents.rag.Events.LimitChatHistoryWithContextEvent import LimitChatHistoryWithContextEvent
 from aihub_agent.agents.rag.Events.StandaloneQuestionCondenserEvent import StandaloneQuestionCondenserEvent
 from aihub_agent.displayers.EventDisplayer import EventDisplayer
+from aihub_agent.workflow.annotations.custom_types.ListOfSize import FixedList
 from aihub_agent.workflow.decorators.step import step
+from aihub_lib.generative_ai.utils.combine_nodes_in_order import combine_nodes_in_order
+from aihub_lib.generative_ai.utils.condense_standalone_question import condense_standalone_question
+from aihub_lib.generative_ai.utils.decompose_chat_history import decompose_chat_history
+from aihub_lib.generative_ai.utils.limit_chat_history import limit_chat_history
+from aihub_lib.generative_ai.utils.limit_chat_history_with_context import limit_chat_history_with_context
+from aihub_lib.generative_ai.utils.retrieve_nodes import retrieve_nodes
+from aihub_lib.i18n.LocaleHandler import LocaleHandler
+from aihub_lib.nats.context.run.RunContext import RunContext
+from aihub_lib.nats.events import StopEvent, LLMEvent
+from aihub_lib.nats.events.control.start import StartEvent
+from aihub_lib.nats.events.semantic.retriever import RetrieverEvent
+from aihub_lib.nats.events.user import UserMessageEvent
 
 
-class MultiHopRAGAgent(RAGAgent):
+class MultiHopRAGAgent(Agent):
     """
-    Builds on the Retrieval-Augmented Generation (RAG) Agent.
+    Implements a Multi Hop Retrieval-Augmented Generation (RAG) Agent.
 
     The MultiHopRAGAgent orchestrates steps to process user input, retrieve relevant information,
     condense questions, and generate responses using a configured language model and retrieval setup.
@@ -44,66 +48,82 @@ class MultiHopRAGAgent(RAGAgent):
     """
 
     @step()
-    async def condense_standalone_question_step(
+    async def limit_chat_history_step(
+        self,
+        event: StartEvent | UserMessageEvent,
+        agent_config: MultiHopRAGAgentConfig,
+        run_context: RunContext,
+    ) -> LimitChatHistoryEvent:
+        """
+        Truncates incoming chat messages to fit within the configured token limit
+        """
+        user_messages = [msg for msg in event.messages if msg.role == MessageRole.USER]
+        try:
+            await run_context.set("user_query", user_messages[-1].content)
+        except IndexError:
+            raise ValueError("No user messages found in the event.")
+        limited_chat_history = limit_chat_history(
+            chat_history=event.messages,
+            number_of_input_tokens=agent_config.number_of_input_tokens,
+        )
+        serialized_chat_history = [msg.model_dump() for msg in limited_chat_history]
+        await run_context.set("chat_history", serialized_chat_history)
+
+        return LimitChatHistoryEvent(limited_history=limited_chat_history)
+
+    @step()
+    async def decompose_query_step(
         self,
         event: LimitChatHistoryEvent,
-        agent_config: RAGAgentConfig,
+        agent_config: MultiHopRAGAgentConfig,
         t: LocaleHandler,
         displayer: EventDisplayer,
         run_context: RunContext,
-    ) -> StandaloneQuestionCondenserEvent:
+    ) -> List[DecomposeQueryEvent]:
         """
         Condenses the chat history and user query into a standalone question.
         """
         await displayer.display_thought(t("agent.thought.condense_question"))
         user_query = await run_context.get("user_query")
         async with agent_config.llm.cost_reporting_llm(displayer) as llm:
-            condensed_question = condense_standalone_question(
+            decomposed_chat_history = decompose_chat_history(
                 chat_history=event.limited_history,
                 message=user_query,
                 t=t,
                 llm=llm,
-                condense_prompt=agent_config.condense_question_prompt,
+                hops=agent_config.hops,
+                decompose_prompt=agent_config.decompose_condense_prompt,
             )
-            return StandaloneQuestionCondenserEvent(condensed_chat_message=condensed_question)
-
-    @step()
-    async def decompose_standalone_question_step(
-        self,
-        event: StandaloneQuestionCondenserEvent,
-        displayer: EventDisplayer,
-        t: LocaleHandler,
-    ) -> RetrieverEvent:
-        """
-        Decomposes the standalone question into sub-questions.
-        """
-        # TODO: add llm to query for decomposition
-        pass
+            return [
+                DecomposeQueryEvent(decomposed_chat_history=decomposed_chat)
+                for decomposed_chat in decomposed_chat_history
+            ]
 
     @step()
     async def retrieve_step(
         self,
-        event: StandaloneQuestionCondenserEvent,
-        retrieve_step_config: RetrieveStepConfig,
+        event: DecomposeQueryEvent,
+        agent_config: MultiHopRAGAgentConfig,
         displayer: EventDisplayer,
         t: LocaleHandler,
-    ) -> ConcatenationEvent:
+    ) -> RetrieverEvent:
         """
         Retrieves relevant nodes from the knowledge base.
         """
         await displayer.display_thought(t("agent.thought.searching_knowledge"))
-        embedding, _ = retrieve_step_config.embed_model.to_llama_index()
+        embedding, _ = agent_config.retrieve_step_config.embed_model.to_llama_index()
         nodes = retrieve_nodes(
             message=event.condensed_chat_message.content,
-            index_name=retrieve_step_config.index_name,
-            index_namespaces=retrieve_step_config.index_namespaces,
-            query_mode=retrieve_step_config.query_mode,
-            node_types=retrieve_step_config.node_types,
-            retrieve_k=retrieve_step_config.retrieve_k,
+            index_name=agent_config.retrieve_step_config.index_name,
+            index_namespaces=agent_config.retrieve_step_config.index_namespaces,
+            query_mode=agent_config.retrieve_step_config.query_mode,
+            node_types=agent_config.retrieve_step_config.node_types,
+            retrieve_k=agent_config.retrieve_step_config.retrieve_k,
             embed_model=embedding,
         )
-        return ConcatenationEvent(nodes)
+        return RetrieverEvent.from_nodes(nodes)
 
+    # TODO: how do I make the fixed list size dynamic?
     @step()
     async def concatenation_step(self, events: FixedList(ConcatenationEvent, 5)) -> RetrieverEvent:
         """
@@ -113,3 +133,69 @@ class MultiHopRAGAgent(RAGAgent):
         [nodes.extend(event.nodes) for event in events]
 
         return RetrieverEvent.from_nodes(nodes)
+
+    @step()
+    async def order_nodes_by_documents_step(
+        self,
+        event: RetrieverEvent,
+        t: LocaleHandler,
+        agent_config: MultiHopRAGAgentConfig,
+        displayer: EventDisplayer,
+    ) -> InOrderNodeCombinerEvent:
+        """
+        Orders the retrieved nodes based on their source documents.
+        """
+        await displayer.display_thought(t("agent.thought.searching_knowledge"))
+        ordered_nodes = combine_nodes_in_order(
+            context_nodes=event.documents,
+            locale_handler=t,
+            context_prompt=agent_config.context_prompt,
+        )
+        return InOrderNodeCombinerEvent(context_message=ordered_nodes)
+
+    @step()
+    async def limit_chat_history_with_context_step(
+        self,
+        event: InOrderNodeCombinerEvent,
+        agent_config: MultiHopRAGAgentConfig,
+        run_context: RunContext,
+    ) -> LimitChatHistoryWithContextEvent:
+        """
+        Includes the combined context and truncates chat history again.
+        """
+        serialized_chat_history = await run_context.get("chat_history")
+        chat_history = [ChatMessage.model_validate(msg) for msg in serialized_chat_history]
+
+        system_messages = [msg for msg in chat_history if msg.role == MessageRole.SYSTEM]
+        last_user_message = await run_context.get("user_query")
+        limited_chat_history = limit_chat_history_with_context(
+            chat_history=chat_history,
+            context_messages=[event.context_message],
+            system_messages=system_messages,
+            last_user_message=ChatMessage(role=MessageRole.USER, content=last_user_message),
+            tokenizer_for_model=agent_config.llm.name,
+            number_of_input_tokens=agent_config.number_of_input_tokens,
+        )
+        return LimitChatHistoryWithContextEvent(limited_history_with_context=limited_chat_history)
+
+    @step()
+    async def respond_with_llm_step(
+        self,
+        event: LimitChatHistoryWithContextEvent,
+        agent_config: MultiHopRAGAgentConfig,
+        displayer: EventDisplayer,
+        t: LocaleHandler,
+    ) -> LLMEvent:
+        """
+        Generates a response using the configured LLM.
+        """
+        await displayer.display_thought(t("agent.thought.write_answer_based_on_information"))
+        async with agent_config.llm.cost_reporting_llm(displayer) as llm:
+            return await displayer.display_llm_stream(agent_config.llm, llm, event.limited_history_with_context)
+
+    @step()
+    async def stop_step(self, event: LLMEvent) -> StopEvent:
+        """
+        Signals the completion of the workflow.
+        """
+        return StopEvent()
