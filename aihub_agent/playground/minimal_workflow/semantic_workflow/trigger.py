b2a92c6d3ede648e3300c96ac48d95c356474ce7
Noah Hermann <91084852+SkypeForBusiness@users.noreply.github.com>
2025-01-24 16:46:16 +0100

    test(aihub): SemanticEventAgent test (#53)
    * initial commit
    
    * all event types handled
    
    * trigger not working
    
    * fixture not found
    
    * working Semantic Event Agent test
    
    * checking number of documents retrieved
    
    * suggested changes from PR
    
    ---------
    
    Co-authored-by: noahhermann <noah.hermann@bbv.ch>

--- aihub_agent/playground/minimal_workflow/semantic_workflow/trigger.py
+++ aihub_agent/playground/minimal_workflow/semantic_workflow/trigger.py
@@ -3,6 +3,7 @@ import asyncio
 from aihub_agent.runners.AgentTestRunner import AgentTestRunner
 from aihub_lib.i18n.LocaleString import LocaleString
 from aihub_lib.nats.events import StartEvent
+from llama_index.core.base.llms.types import ChatMessage, MessageRole
 from playground.minimal_workflow.semantic_workflow.SemanticEventAgent import (
     SemanticEventAgent,
 )
@@ -23,7 +24,10 @@ async def main():
     )
 
     async with runner.test_run() as topic:
-        await runner.send_event_from_topic(topic=topic, start_event=StartEvent(messages=[]))
+        await runner.send_event_from_topic(
+            topic=topic,
+            start_event=StartEvent(messages=[ChatMessage(content="Hello", role=MessageRole.USER)]),
+        )
 
 
 if __name__ == "__main__":
