65d714232d57c9dc801f02b831179d7aaec7920e
mariushoegger <marius-hoegger@bbv.ch>
2025-01-29 10:02:15 +0100

    update poetry.lock

--- aihub_agent/aihub_agent/agents/rag/Configs/RAGAgentConfig.py
+++ aihub_agent/aihub_agent/agents/rag/Configs/RAGAgentConfig.py
@@ -3,7 +3,7 @@ from aihub_lib.generative_ai.llms.models.chat.ChatLLMConfig import ChatLLMConfig
 from aihub_lib.i18n.LocaleString import LocaleString
 from pydantic import Field
 
-from aihub_agent.aihub_agent.agents.rag.Configs.RetrieveStepConfig import RetrieveStepConfig
+from aihub_agent.agents.rag.Configs.RetrieveStepConfig import RetrieveStepConfig
 
 
 class RAGAgentConfig(AgentConfig):
