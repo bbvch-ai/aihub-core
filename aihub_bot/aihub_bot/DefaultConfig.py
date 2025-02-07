2e55b6c444482d24c92eff1ba0e0ffe3028e2eeb
thommann <thomas_m@hotmail.ch>
2025-02-07 12:35:06 +0100

    azure bot service config

--- aihub_bot/aihub_bot/DefaultConfig.py
+++ aihub_bot/aihub_bot/DefaultConfig.py
@@ -6,6 +6,5 @@ import os
 class DefaultConfig:
     """Bot Configuration"""
 
-    PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "9b10ea6a-4068-45b9-bbe4-1ffc2a718a6b")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "oa~8Q~euboz2HcVu2RK.b3JUh0IbIesV6OG0IcP5")
