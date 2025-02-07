141b931ee7a191277c3b71d332571849bd396a4c
thommann <thomas_m@hotmail.ch>
2025-02-07 15:50:31 +0100

    send all messages in user message event

--- aihub_lib/aihub_lib/routes/chat/ChatService.py
+++ aihub_lib/aihub_lib/routes/chat/ChatService.py
@@ -63,8 +63,7 @@ class ChatService:
             thread_id=str(thread.id),
             display_id=str(ObjectId()),
             event=UserMessageEvent(
-                messages=messages[:-1],
-                content=messages[-1].content,
+                messages=messages,
             ),
         )
         logger.debug(f"Created event: {event}")
