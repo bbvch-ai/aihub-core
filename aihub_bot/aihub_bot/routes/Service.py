03801bbf3bf68e6a6be5c91ce87a15879117f689
thommann <thomas_m@hotmail.ch>
2025-02-14 16:52:46 +0100

    remove on turn arror

--- aihub_bot/aihub_bot/routes/Service.py
+++ aihub_bot/aihub_bot/routes/Service.py
@@ -1,10 +1,7 @@
 import abc
 import logging
-from datetime import datetime
 
-from botbuilder.core import TurnContext
 from botbuilder.integration.aiohttp import CloudAdapter, ConfigurationBotFrameworkAuthentication
-from botbuilder.schema import Activity, ActivityTypes
 
 from aihub_bot.persistence.entities.PathEntity import Credentials, PathEntity
 
@@ -16,23 +13,3 @@ class Service(abc.ABC):
     def get_adapter(path: str) -> CloudAdapter:
         credentials: Credentials = PathEntity.get_credentials_by_path(path)
         return CloudAdapter(ConfigurationBotFrameworkAuthentication(credentials))
-
-    @staticmethod
-    async def on_error(context: TurnContext, error: Exception):
-        logger.error(f"\n [on_turn_error] unhandled error: {error}")
-
-        await context.send_activity("The bot encountered an error or bug.")
-
-        # Send a trace activity if we're talking to the Bot Framework Emulator
-        if context.activity.channel_id == "emulator":
-            trace_activity = Activity(
-                label="TurnError",
-                name="on_turn_error Trace",
-                timestamp=datetime.now(),
-                type=ActivityTypes.trace,
-                value=f"{error}",
-                value_type="https://www.botframework.com/schemas/error",
-            )
-            await context.send_activity(trace_activity)
-
-    ADAPTER.on_turn_error = on_error
