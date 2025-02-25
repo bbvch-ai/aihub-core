14af5617de6043840f726c0477c5cd2e882854dd
Thomas <34217413+thommann@users.noreply.github.com>
2025-02-25 12:16:52 +0100

    fix(aihub): Allow self hosted llm in llm wrapping agent (#166)

--- aihub_agent/aihub_agent/agents/basic/LLMWrappingAgent/LLMWrappingAgentConfig.py
+++ aihub_agent/aihub_agent/agents/basic/LLMWrappingAgent/LLMWrappingAgentConfig.py
@@ -1,6 +1,6 @@
 from aihub_lib.agents.AgentConfig import AgentConfig
-from aihub_lib.generative_ai.resources.models.llm.chat.azure.AzureOpenAILLMConfig import AzureOpenAILLMConfig
+from aihub_lib.generative_ai.resources.models.llm.chat.ChatLLMConfig import ChatLLMConfig
 
 
 class LLMWrappingAgentConfig(AgentConfig):
-    llm: AzureOpenAILLMConfig
+    llm: ChatLLMConfig
