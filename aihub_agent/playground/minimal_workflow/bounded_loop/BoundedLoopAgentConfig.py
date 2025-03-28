6fb0ffd386cd3b741898893d3eef509206442df5
Noah Hermann <91084852+SkypeForBusiness@users.noreply.github.com>
2025-03-28 10:05:18 +0100

    feat(workflows): Bounded Loop Agent (#189)
    * workflow that represents a loop which can be executed a set amount of times
    
    * test for bounded loop agent
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * pr-ready
    
    * pr suggestion
    
    * pr suggestions
    
    ---------
    
    Co-authored-by: noahhermann <noah.hermann@bbv.ch>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ aihub_agent/playground/minimal_workflow/bounded_loop/BoundedLoopAgentConfig.py
@@ -0,0 +1,5 @@
+from aihub_lib.agents.AgentConfig import AgentConfig
+
+
+class BoundedLoopAgentConfig(AgentConfig):
+    loop_max: int
---
+++ aihub_agent/playground/minimal_workflow/bounded_loop/events/ProcessEvent.py
@@ -0,0 +1,5 @@
+from aihub_lib.nats.events import ControlEvent
+
+
+class ProcessEvent(ControlEvent):
+    pass
