1345ce430bc75ce1d5584735241428426fd53594
Michèle Fundneider <34126539+mfundn@users.noreply.github.com>
2025-04-10 10:29:54 +0200

    make pr ready

--- aihub_pipeline/aihub_pipeline/types/RefDocDocument.py
+++ aihub_pipeline/aihub_pipeline/types/RefDocDocument.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 from typing import TYPE_CHECKING, Optional
 
-from llama_index.core import Document
-from pydantic import computed_field
-
 from aihub_lib.persistence.rag.vectors.node_metadata import (
     CREATED_AT,
     DATA_LAKE_URI,
@@ -16,6 +13,8 @@ from aihub_lib.persistence.rag.vectors.node_metadata import (
     TYPE,
     UPDATED_AT,
 )
+from llama_index.core import Document
+from pydantic import computed_field
 
 if TYPE_CHECKING:
     from aihub_pipeline.types.DataLakeFile import DataLakeFile
