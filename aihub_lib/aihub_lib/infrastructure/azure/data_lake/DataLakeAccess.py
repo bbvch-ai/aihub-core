8b3c323dea830e28f71b3b89274dc8b3cd11a5c1
Michèle Fundneider <34126539+mfundn@users.noreply.github.com>
2025-05-13 16:57:29 +0200

    cache fs

--- aihub_lib/aihub_lib/infrastructure/azure/data_lake/DataLakeAccess.py
+++ aihub_lib/aihub_lib/infrastructure/azure/data_lake/DataLakeAccess.py
@@ -1,7 +1,6 @@
-from typing import cast
+from typing import Optional
 
 from adlfs import AzureBlobFileSystem
-from azure.core.credentials import TokenCredential
 from azure.identity import DefaultAzureCredential
 from azure.storage.filedatalake import DataLakeServiceClient
 
@@ -16,6 +15,7 @@ class DataLakeAccess:
     _storage_service_name = None
     _service_endpoint = None
     _credential = None
+    _cached_fs_client: Optional[AzureBlobFileSystem] = None
 
     def __new__(cls):
         if cls._instance is None:
@@ -31,20 +31,20 @@ class DataLakeAccess:
             DataLakeConfig().DATA_LAKE_ENDPOINT or f"https://{self._storage_service_name}.dfs.core.windows.net"
         )
 
-        self._credential = cast(TokenCredential, DefaultAzureCredential())
+        self._credential = DefaultAzureCredential()
         self.datalake_client = DataLakeServiceClient(
             account_url=self._service_endpoint,
             credential=self._credential,
         )
+        self._cached_fs_client = None
 
     def get_client(self) -> DataLakeServiceClient:
         return self.datalake_client
 
     def get_fs_client(self) -> AzureBlobFileSystem:
-        return AzureBlobFileSystem(
-            account_name=self._storage_service_name,
-            token_credential=self._credential
-        )
+        if self._cached_fs_client is None:
+            self._cached_fs_client = AzureBlobFileSystem(account_name=self._storage_service_name)
+        return self._cached_fs_client
 
     def get_storage_account_name(self) -> str:
         return self._storage_service_name
