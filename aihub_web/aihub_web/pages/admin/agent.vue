311e943c428dfbb4b3e93a0e1f9d2a505e89e097
Joel Barmettler <joel.barmettler@gmail.com>
2025-05-22 08:10:06 +0200

    remove dead code

--- aihub_web/aihub_web/pages/admin/agent.vue
+++ aihub_web/aihub_web/pages/admin/agent.vue
@@ -3,7 +3,6 @@
     <StructuralColumn
       :title="t('agent.title')"
       :loading="agentsAreLoading"
-      :nav-items="navItems"
     >
       <div
         class="grid grid-cols-2 gap-4 2xl:grid-cols-2"
@@ -37,23 +36,4 @@ const { agents, agentsAreLoading } = useAgents()
 const toAgent = (agent: AgentDto) => {
   router.push(localePath(`/admin/agent/agent-${agent.agent_id}-${agent.agent_class}/overview`))
 }
-
-const navItems = computed<Record<string, NavItem[]>>(() => {
-  if (agentsAreLoading.value) {
-    return {}
-  }
-  const typeMap: Record<string, NavItem[]> = {}
-  agents.value?.forEach((agent: AgentDto) => {
-    if (!(agent.agent_class in typeMap)) {
-      typeMap[agent.agent_class] = []
-    }
-    typeMap[agent.agent_class].push({
-      name: agent.agent_config.name,
-      key: `${agent.agent_class}${agent.agent_id}`,
-      path: `/admin/agent/agent-${agent.agent_id}-${agent.agent_class}/overview`,
-      isActive: () => route.params.agent_id === agent.agent_id && route.params.agent_class === agent.agent_class,
-    })
-  })
-  return typeMap
-})
 </script>
