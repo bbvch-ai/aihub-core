e640079026b9ee3630618531d086bafdf88bee35
mariushoegger <marius-hoegger@bbv.ch>
2025-05-30 11:45:05 +0200

    fix PR

--- aihub_iac/aihub_iac/azure/modules/stores/StoresConfig.py
+++ aihub_iac/aihub_iac/azure/modules/stores/StoresConfig.py
@@ -1,43 +1,1 @@
-from typing import ClassVar
-
-from pydantic import Field
-
-from aihub_iac.azure.resources.BaseConfig import BaseConfig
-from aihub_iac.azure.settings.PostgresAuthSettings import PostgresAuthSettings
-
-
-class StoresConfig(BaseConfig):
-    """Configuration class for Nats infrastructure"""
-
-    _postgres_settings: ClassVar[PostgresAuthSettings] = PostgresAuthSettings()
-
-    DEFAULT_DOCSTORE_SUFFIX: ClassVar[str] = "docstore"
-    API_COSMOS_SUBNET_CIDR: ClassVar[str] = "10.0.37.0/24"
-    SEARCH_SUBNET_CIDR: ClassVar[str] = "10.0.34.0/24"
-    PG_SUBNET_CIDR: ClassVar[str] = "10.0.4.0/24"
-    COSMOS_SUBNET_CIDR: ClassVar[str] = "10.0.33.0/24"
-
-    postgres_username: str = Field(
-        default_factory=lambda: StoresConfig._postgres_settings.POSTGRES_USERNAME,
-        description="Username for the PostgreSQL database",
-    )
-    postgres_password: str = Field(
-        default_factory=lambda: StoresConfig._postgres_settings.POSTGRES_PASSWORD,
-        description="Password for the PostgreSQL database",
-    )
-
-    @property
-    def ai_search_service_name_force(self) -> str:
-        return self.resource_namer.ai_search_name()
-
-    @property
-    def doc_store_name(self) -> str:
-        return self.resource_namer.cosmos_name(StoresConfig.DEFAULT_DOCSTORE_SUFFIX)
-
-    @property
-    def store_name(self) -> str:
-        return self.resource_namer.cosmos_name()
-
-    @property
-    def postgres_name(self) -> str:
-        return self.resource_namer.postgres_name()
+from typing import ClassVarfrom pydantic import Fieldfrom aihub_iac.azure.resources.BaseConfig import BaseConfigfrom aihub_iac.azure.settings.PostgresAuthSettings import PostgresAuthSettingsclass StoresConfig(BaseConfig):    """Configuration class for Nats infrastructure"""    _postgres_settings: ClassVar[PostgresAuthSettings] = PostgresAuthSettings()    DEFAULT_DOCSTORE_SUFFIX: ClassVar[str] = "docstore"    API_COSMOS_SUBNET_CIDR: ClassVar[str] = "10.0.37.0/24"    SEARCH_SUBNET_CIDR: ClassVar[str] = "10.0.34.0/24"    PG_SUBNET_CIDR: ClassVar[str] = "10.0.4.0/24"    COSMOS_SUBNET_CIDR: ClassVar[str] = "10.0.33.0/24"    postgres_username: str = Field(        default_factory=lambda: StoresConfig._postgres_settings.POSTGRES_USERNAME,        description="Username for the PostgreSQL database",    )    postgres_password: str = Field(        default_factory=lambda: StoresConfig._postgres_settings.POSTGRES_PASSWORD,        description="Password for the PostgreSQL database",    )    @property    def ai_search_service_name(self) -> str:        return self.resource_namer.ai_search_name()    @property    def doc_store_name(self) -> str:        return self.resource_namer.cosmos_name(StoresConfig.DEFAULT_DOCSTORE_SUFFIX)    @property    def store_name(self) -> str:        return self.resource_namer.cosmos_name()    @property    def postgres_name(self) -> str:        return self.resource_namer.postgres_name()
