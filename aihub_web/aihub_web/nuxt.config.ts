a07a645d449dbe3992e7d97920932683a290d47e
Joel Barmettler <joel.barmettler@gmail.com>
2025-06-10 16:44:59 +0200

    fix vueuse

--- aihub_web/aihub_web/nuxt.config.ts
+++ aihub_web/aihub_web/nuxt.config.ts
@@ -31,21 +31,6 @@ export default defineNuxtConfig({
     fileURLToPath(new URL('./assets/css/main.css', import.meta.url)),
   ],
 
-  // You must configure the client application in azure accordingly
-  // https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Authentication/appId/<<<CLIENT_ID>>>/isMSAApp~/false
-  // Register the following Single-page application Redirect URIs
-  // http://localhost:8080/<<<LOCALE>>>/auth/refresh
-  // http://localhost:8080/<<<LOCALE>>>/auth/callback
-  // For all locales that you support
-  runtimeConfig: {
-    public: {
-      oidc: {
-        clientId: '3dc76991-ddd7-4c2d-9cf9-ac2146f33d23',
-        tenantId: '279985bd-2077-4d9d-9797-42238cfc06e2',
-      },
-    },
-  },
-
   alias: {
     '@core': fileURLToPath(new URL('./', import.meta.url)),
   },
