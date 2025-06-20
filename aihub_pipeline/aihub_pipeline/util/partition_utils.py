cd6c4ff945c15012ed2cf0dd3ec39376f91152bd
Michele Fundneider <34126539+mfundn@users.noreply.github.com>
2025-06-20 09:58:15 +0200

    fix(dagster): Optimize partition key replacement logic (#396)

--- aihub_pipeline/aihub_pipeline/util/partition_utils.py
+++ aihub_pipeline/aihub_pipeline/util/partition_utils.py
@@ -4,10 +4,18 @@ from dagster import OpExecutionContext
 
 
 def replace_partition_keys(context: OpExecutionContext, partition_name: str, keys: List[str]):
-    old_partition_keys = context.instance.get_dynamic_partitions(partition_name)
-    for old_partition_key in old_partition_keys:
-        context.instance.delete_dynamic_partition(partitions_def_name=partition_name, partition_key=old_partition_key)
-    context.instance.add_dynamic_partitions(
-        partitions_def_name=partition_name,
-        partition_keys=keys,
-    )
+    new_keys_set = set(keys)
+    old_partition_keys_set = set(context.instance.get_dynamic_partitions(partition_name))
+
+    partitions_to_add = list(new_keys_set - old_partition_keys_set)
+    partitions_to_delete = list(old_partition_keys_set - new_keys_set)
+
+    if partitions_to_add:
+        context.instance.add_dynamic_partitions(
+            partitions_def_name=partition_name,
+            partition_keys=partitions_to_add,
+        )
+
+    if partitions_to_delete:
+        for partition_key in partitions_to_delete:
+            context.instance.delete_dynamic_partition(partitions_def_name=partition_name, partition_key=partition_key)
