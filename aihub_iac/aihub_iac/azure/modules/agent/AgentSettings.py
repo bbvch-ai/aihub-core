195cdc48e61346240cab99ff749f9561b9e5dbfb
Michèle Fundneider <34126539+mfundn@users.noreply.github.com>
2025-06-23 15:47:46 +0200

    Remove URL_SIGNING_SECRET from AgentSettings to streamline configuration

--- aihub_iac/aihub_iac/azure/modules/agent/AgentSettings.py
+++ aihub_iac/aihub_iac/azure/modules/agent/AgentSettings.py
@@ -8,4 +8,3 @@ class AgentSettings(BaseSettings):
     AGENT_REPO_IMAGE_URL: Optional[str] = Field(default=None, description="URL where the image for the agent is stored")
     AGENT_IMAGE_TAG: Optional[str] = Field(default=None, description="image tag for the agent")
     AGENT_PHOENIX_API_TOKEN: Optional[str] = Field(default=None, description="API Token for Phoenix")
-    URL_SIGNING_SECRET: Optional[str] = Field(default=None, description="A secret key used for signing and verifying temporary anonymous access URLs")
