7662b6d26c10999bb377417763b010badffd866f
Joel Barmettler <joel.barmettler@gmail.com>
2025-08-11 18:55:37 +0200

    **refactor(secrets): Update `AUTH_TOKEN` and `CONNECTION_STRING` handling for improved security and consistency**
    - Replaced direct field access of `AUTH_TOKEN` with `.get_secret_value()` to align with secure handling practices.
    - Changed `MongoSettings.CONNECTION_STRING` from optional to required for streamlined configurations.
    - Simplified Phoenix headers generation to improve maintainability.

--- aihub_lib/aihub_lib/infrastructure/mongo/MongoSettings.py
+++ aihub_lib/aihub_lib/infrastructure/mongo/MongoSettings.py
@@ -8,4 +8,4 @@ from aihub_lib.settings.EnvironmentSettings import EnvironmentSettings
 class MongoSettings(EnvironmentSettings):
     model_config = EnvironmentSettings.create_settings_config("MONGO_")
 
-    CONNECTION_STRING: Annotated[SecretStr | None, Field(description="Overwrite the MongoDB connection string")] = None
+    CONNECTION_STRING: Annotated[SecretStr, Field(description="Overwrite the MongoDB connection string")]
