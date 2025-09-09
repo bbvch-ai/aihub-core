a72b0a195d762a14401f6f0586c31d1133fd1ee8
mariushoegger <marius-hoegger@bbv.ch>
2025-09-09 15:38:49 +0200

    delete focusday agents

--- aihub_agent/my_custom_agent/MyCustomAgent/events/MyCustomAgentEvent.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from aihub_lib.nats.events import ControlEvent
-from pydantic import Field
-
-
-class MyCustomAgentEvent(ControlEvent):
-    word_count: Annotated[int, Field(description="The word count of the processed content")]
