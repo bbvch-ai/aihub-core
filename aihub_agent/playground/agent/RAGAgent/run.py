f000aec6805383999f772e2ae8507b7fd0de7076
noahhermann <noah.hermann@bbv.ch>
2025-09-18 09:02:54 +0200

    changing configs to accommodate cpu and gpu docker compose

--- aihub_agent/playground/agent/RAGAgent/run.py
+++ aihub_agent/playground/agent/RAGAgent/run.py
@@ -34,7 +34,7 @@ async def main():
                 fr="Ceci est l'agent RAG par défaut",
                 it="Questo è l'agente RAG predefinito",
             ),
-            llm=LLMConfig(model_name="local/gemma-3-multimodal-small"),
+            llm=LLMConfig(model_name="local/qwen3-small"),
             check_context_sufficiency=False,
             number_of_input_tokens=12000,
             system_prompt=LocaleString(
