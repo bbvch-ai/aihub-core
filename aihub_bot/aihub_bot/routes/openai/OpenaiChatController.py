4cf0f7560a014f6cf854a148a59e05a707c59701
Thomas Mannhart <thomas.mannhart@bbv.ch>
2025-10-01 17:39:20 +0200

    Refactor `RoutesService` connection management and integrate OpenAI async client in `OpenaiChatController`.

--- aihub_bot/aihub_bot/routes/openai/OpenaiChatController.py
+++ aihub_bot/aihub_bot/routes/openai/OpenaiChatController.py
@@ -1,6 +1,8 @@
 import logging
 from typing import Annotated
 
+import openai
+
 from aihub_lib.auth.dependencies.AuthHandler import AuthHandler
 from aihub_lib.auth.identity.UserIdentity import UserIdentity
 from aihub_lib.generative_ai.resources.models.llm.LLMConfig import LLMConfig
@@ -49,10 +51,9 @@ class OpenaiChatController(Controller):
         async def json_chat_completion(
             request: Request,
             user: Annotated[UserIdentity, Security(self.user_with_permission("aihub.user.?>"))],
-            _: Annotated[ActivityModel, Body],
             model_name: Annotated[str, Query(title="Model Name")],
         ) -> Response:
-            client = LiteLLMService.openai_aclient_for_user(user=user)
+            client: openai.AsyncClient = await LiteLLMService.openai_aclient_for_user(user=user)
 
             path = RoutesService.get_path(request)
 
