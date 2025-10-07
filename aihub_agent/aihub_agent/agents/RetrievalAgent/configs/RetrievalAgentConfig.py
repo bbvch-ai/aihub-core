46022518f8bf671899edae7d660abbf783ea94a3
Michèle Fundneider <34126539+mfundn@users.noreply.github.com>
2025-10-07 16:19:35 +0200

    feat(bucket_utils): Refactor bucket and namespace creation logic

--- aihub_agent/aihub_agent/agents/RetrievalAgent/configs/RetrievalAgentConfig.py
+++ aihub_agent/aihub_agent/agents/RetrievalAgent/configs/RetrievalAgentConfig.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from aihub_lib.agents.AgentConfig import AgentConfig
+from aihub_lib.i18n.LocaleString import LocaleString
 from pydantic import Field
 
 from aihub_agent.agents.RagAgent.configs.RetrieveStepConfig import RetrieveStepConfig
@@ -10,3 +11,6 @@ class RetrievalAgentConfig(AgentConfig):
     retrieve_step_config: Annotated[
         RetrieveStepConfig, Field(..., description="The configuration for the retrieval step.")
     ]
+    context_prompt: Annotated[LocaleString | None, Field(description="The context prompt for the retrieval step.")] = (
+        None
+    )
