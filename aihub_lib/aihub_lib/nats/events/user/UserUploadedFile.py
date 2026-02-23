532a2bc325d459afc066ec77d0b3405ad9bc9b8a
Michele Fundneider <34126539+mfundn@users.noreply.github.com>
2026-02-23 16:44:12 +0100

    Remove S3 file fetch logic from `UserUploadedFile` class

--- aihub_lib/aihub_lib/nats/events/user/UserUploadedFile.py
+++ aihub_lib/aihub_lib/nats/events/user/UserUploadedFile.py
@@ -1,11 +1,6 @@
-import asyncio
 from typing import Annotated
 
-import boto3
-from botocore.config import Config
 from pydantic import BaseModel, Field
-
-from aihub_lib.infrastructure.s3.S3StorageSettings import S3StorageSettings
 
 
 class UserUploadedFile(BaseModel):
@@ -15,21 +10,3 @@ class UserUploadedFile(BaseModel):
     ]
     s3_bucket: Annotated[str, Field(description="The S3 bucket where the file is stored.")]
     s3_key: Annotated[str, Field(description="The S3 object key for the file.")]
-
-    async def fetch_content(self) -> bytes:
-        """Download the file content from S3 on demand."""
-        settings = S3StorageSettings()
-        client = boto3.client(
-            "s3",
-            endpoint_url=settings.ENDPOINT,
-            aws_access_key_id=settings.ACCESS_KEY,
-            aws_secret_access_key=settings.SECRET_KEY.get_secret_value(),
-            region_name=settings.REGION,
-            config=Config(signature_version="s3v4"),
-        )
-
-        def _download() -> bytes:
-            response = client.get_object(Bucket=self.s3_bucket, Key=self.s3_key)
-            return response["Body"].read()
-
-        return await asyncio.to_thread(_download)
