e34f95498d899d11da319e9587a707f3514acab4
Michele Fundneider <34126539+mfundn@users.noreply.github.com>
2026-02-25 10:21:16 +0100

    Update aihub_web/aihub_web/pages/index.vue
    Co-authored-by: Joel Barmettler <joel.barmettler@gmail.com>

--- aihub_web/aihub_web/pages/index.vue
+++ aihub_web/aihub_web/pages/index.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    v-if="isAgentAdmin"
-    class=""
-  >
+  <div v-if="isAgentAdmin">
     <h1 class="w-full pb-32 pt-72 text-center text-6xl">
       {{ t('welcome') }}
     </h1>
