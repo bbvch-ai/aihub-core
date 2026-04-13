813777076741e6a9b8de3d887e61658cae789d5f
mhoegger <marius.hoegger@bbv.ch>
2026-04-13 16:49:44 +0200

    Remove unused `getMyActiveTenant` import from index.vue.

--- packages/web/swiss_ai_hub_web/pages/index.vue
+++ packages/web/swiss_ai_hub_web/pages/index.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script setup lang="ts">
-import { getMyActiveTenant, getMyTenants } from '@core/sdk/client'
+import { getMyTenants } from '@core/sdk/client'
 
 import type { TenantMembershipDto } from '@core/sdk/client'
 
